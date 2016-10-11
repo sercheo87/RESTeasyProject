320152cd1ba9e6f0bad4c5433aca57a65f57d5fd
sercheo87 <chancaysergio@gmail.com>
2016-10-10 22:21:57 -0500

    Se ajusta codigo de anotaciones para servicios rest

---
+++ src/main/java/com/buraktas/application/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.buraktas.application;
+
+import java.util.Collections;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class BaseApplication extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		return Collections.emptySet();
+	}
+}
---
+++ src/main/java/com/buraktas/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.buraktas.helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class HelloWorld {
+
+	@GET
+	@Path("/helloworld")
+	public Response getHelloWorld() {
+		String value = "Hello World";
+		return Response.status(200).entity(value).build();
+	}
+}
